77b35f828c0e70fce17531b83cf11e69c8b7b880
Wild-F1re <52320217+Wild-F1re@users.noreply.github.com>
2020-03-09 04:13:00 +0900

    Jsファイルの追加・hoverアクションの適用

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    $('.skill-item01__icon').hover(
+        function(){
+          $('.skill-item02__icon').fadeOut();
+        },
+        function(){
+          $('.skill-item02__icon').fadeIn();
+        }
+        );
+
+    $('.skill-item02__icon').hover(
+        function(){
+            $('.skill-item01__icon').fadeOut();
+        },
+        function(){
+            $('.skill-item01__icon').fadeIn();
+        }
+        );
+
+  });  
